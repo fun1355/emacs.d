f4db8188e1d5ed0408d06766145941691a7b51ac
sancao2 <linjiangxiandong@gmail.com>
2017-01-01 22:49:38 +0800

    delete old requires

--- init.el
+++ init.el
@@ -23,180 +23,14 @@
 (setq *linux* (or (eq system-type 'gnu/linux) (eq system-type 'linux)) )
 (setq *unix* (or *linux* (eq system-type 'usg-unix-v) (eq system-type 'berkeley-unix)) )
 (setq *emacs24* (and (not (featurep 'xemacs)) (or (>= emacs-major-version 24))) )
-(setq *emacs24old*  (or (and (= emacs-major-version 24) (= emacs-minor-version 3))
-                        (not *emacs24*)))
-
 (setq *no-memory* (cond
                    (*is-a-mac*
                     (< (string-to-number (nth 1 (split-string (shell-command-to-string "sysctl hw.physmem")))) 4000000000))
                    (*linux* nil)
                    (t nil)))
 
-
-;;----------------------------------------------------------------------------
-;; Less GC, more memory
-;;----------------------------------------------------------------------------
-(defun my-optimize-gc (NUM PER)
-"By default Emacs will initiate GC every 0.76 MB allocated (gc-cons-threshold == 800000).
-@see http://www.gnu.org/software/emacs/manual/html_node/elisp/Garbage-Collection.html
-We increase this to 16MB by `(my-optimize-gc 16 0.5)` "
-  (setq-default gc-cons-threshold (* 1024 1024 NUM)
-                gc-cons-percentage PER))
-
-
-(require 'init-modeline)
-(require 'cl-lib)
-(require 'init-compat)
-(require 'init-utils)
-(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
-
-;; win32 auto configuration, assuming that cygwin is installed at "c:/cygwin"
-;; (condition-case nil
-;;     (when *win32*
-;;       ;; (setq cygwin-mount-cygwin-bin-directory "c:/cygwin/bin")
-;;       (setq cygwin-mount-cygwin-bin-directory "c:/cygwin64/bin")
-;;       (require 'setup-cygwin)
-;;       ;; better to set HOME env in GUI
-;;       ;; (setenv "HOME" "c:/cygwin/home/someuser")
-;;       )
-;;   (error
-;;    (message "setup-cygwin failed, continue anyway")
-;;    ))
-
-(require 'idle-require)
-(require 'init-elpa)
-(require 'init-exec-path) ;; Set up $PATH
-;; (require 'init-frame-hooks) ;; cant open load file: init-frame-hooks
-;; any file use flyspell should be initialized after init-spelling.el
-;; actually, I don't know which major-mode use flyspell.
-(require 'init-spelling)
-;; (require 'init-xterm) ;; cant open load file: init-xterm
-(require 'init-gui-frames)
-(require 'init-ido)
-(require 'init-dired)
-(require 'init-uniquify)
-(require 'init-ibuffer)
-;; (require 'init-flymake) ;; cant open load file: init-flymake
-;; (require 'init-smex) ;; cant open load file: init-smex 
-;; (require 'init-helm) ;; cant open load file: init-helm
-(require 'init-hippie-expand)
-(require 'init-windows)
-(require 'init-sessions)
-(require 'init-git)
-(require 'init-crontab)
-(require 'init-markdown)
-(require 'init-erlang)
-(require 'init-javascript)
-(require 'init-org)
-;; (require 'init-org-mime)  ;; cant open load file: init-org-mine
-(require 'init-css)
-;; (require 'init-python-mode) ;; Debugger entered--Lisp error: (error "Command \"ipython\" not found")
-(require 'init-haskell)
-(require 'init-ruby-mode)
-(require 'init-lisp)
-(require 'init-elisp)
-(require 'init-yasnippet)
-;; Use bookmark instead
-;; (require 'init-zencoding-mode) ;; cant open load file: init-org-mine
-(require 'init-cc-mode)
-(require 'init-gud)
-(require 'init-linum-mode)
-(require 'init-moz)
-(require 'init-gtags)
-;; use evil mode (vi key binding)
-;; ;; (Require 'init-evil)
-(require 'init-sh)
-(require 'init-ctags)
-(require 'init-bbdb)
-(require 'init-gnus)
-(require 'init-lua-mode)
-(require 'init-workgroups2)
-(require 'init-term-mode)
-(require 'init-web-mode)
-(require 'init-slime)
-(when *emacs24* (require 'init-company))
-;; (require 'init-stripe-buffer) ;; a bug for me
-(require 'init-ocaml) ;; Args out of rangs "", 0, -1
-(require 'init-clojure)
-;; (require 'init-scheme) ;; a bug for me
-;; (require 'init-eim) ;;  cannot be idle-required ;; cant open load file: init-eim
-(require 'init-hs-minor-mode)
-(require 'init-web-mode)
-(require 'init-slime)
-(require 'init-clipboard)
-(require 'init-company)
-(require 'init-chinese-pyim)
-;; ;; cannot be idle-required
-;; ;; need statistics of keyfreq asap
-;; (require 'init-keyfreq)
-(require 'init-httpd)
-
-;; ;; projectile costs 7% startup time
-
-;; ;; misc has some crucial tools I need immediately
-;; (require 'init-misc) ;; a bug for me
-
-
-;; comment below line if you want to setup color theme in your own way
-(if (or (display-graphic-p) (string-match-p "256color"(getenv "TERM"))) (require 'init-color-theme))
-
-(require 'init-emacs-w3m)
-
-;; color theme
-;; (require 'color-theme)
-;; (require 'color-theme-molokai)
-;; (color-theme-molokai)
-;;(require 'init-color-theme)
-
-;; misc has some crucial tools I need immediately
-;; (require 'init-misc)
-
-(setq idle-require-idle-delay 3)
-(setq idle-require-symbols '(init-writing
-                             init-elnode
-;; {{ idle require other stuff
-(setq idle-require-idle-delay 3)
-(setq idle-require-symbols '(init-misc-lazy
-                             init-which-func
-                             init-fonts
-                             init-hs-minor-mode
-                             init-textile
-                             init-csv
-                             init-writting
-                             init-doxygen
-                             init-pomodoro
-                             init-emacspeak
-                             init-artbollocks-mode
-                             init-semantic))
-(idle-require-mode 1) ;; starts loading
-;; }}
-
-(when (require 'time-date nil t)
-   (message "Emacs startup time: %d seconds."
-    (time-to-seconds (time-since emacs-load-start-time))))
-
-;;----------------------------------------------------------------------------
-;; Locales (setting them earlier in this file doesn't work in X)
-;;----------------------------------------------------------------------------
-(require 'init-locales)
-
-;; my personal setup, other major-mode specific setup need it.
-;; It's dependent on init-site-lisp.el
-(if (file-exists-p "~/.custom.el") (load-file "~/.custom.el"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((lentic-init . lentic-orgel-org-init))))
- '(session-use-package t nil (session)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold))) t))
+(setq *emacs24old*  (or (and (= emacs-major-version 24) (= emacs-minor-version 3))
+                        (not *emacs24*)))
 
 
 ;; *Message* buffer should be writable in 24.4+
@@ -269,7 +103,7 @@ We increase this to 16MB by `(my-optimize-gc 16 0.5)` "
   ;; init-evil dependent on init-clipboard
   (require 'init-clipboard)
   ;; use evil mode (vi key binding)
-  (require 'init-evil)
+  ;; (require 'init-evil)
   (require 'init-multiple-cursors)
   (require 'init-sh)
   (require 'init-ctags)
@@ -280,6 +114,8 @@ We increase this to 16MB by `(my-optimize-gc 16 0.5)` "
   (require 'init-term-mode)
   (require 'init-web-mode)
   (require 'init-slime)
+  (require 'init-ocaml)
+  (require 'init-clojure)
   (require 'init-company)
   (require 'init-chinese-pyim) ;; cannot be idle-required
   ;; need statistics of keyfreq asap
